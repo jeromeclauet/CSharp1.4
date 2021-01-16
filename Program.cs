a8ec3ac1984790833eeb1f1ad212a307796185d5
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 11:01:42 +0100

    Exercice 5

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dites moi votre nom et votre prénom");
+
+            string v1;
+            v1 = Console.ReadLine();
+            string v2;
+            v2 = Console.ReadLine();
+
+            Console.WriteLine("Bonjour, " + v1 + " " + v2 + ", nous sommes le Vendredi 15 Janvier, comment allez-vous? ");
+        }
+    }
+}
